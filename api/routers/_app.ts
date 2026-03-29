49c5ed96674c37d3f8acdc56b6a805cea7a6de0b
Henry Ou <henryouly@gmail.com>
2026-03-29 14:44:31 -0700

    feat: migrate user router to api/

--- api/routers/_app.ts
+++ api/routers/_app.ts
@@ -1,6 +1,7 @@
 import { router, publicProcedure } from "../trpc.js";
 import { episodesRouter } from "./episodes.js";
 import { sourcesRouter } from "./sources.js";
+import { userRouter } from "./user.js";
 
 export const appRouter = router({
   health: publicProcedure.query(() => {
@@ -11,6 +12,7 @@ export const appRouter = router({
   }),
   episodes: episodesRouter,
   sources: sourcesRouter,
+  user: userRouter,
 });
 
 export type AppRouter = typeof appRouter;
